645e716c653f934a5010fe69d8be8dc315812413
Minji Song <minjisong128@gmail.com>
2024-12-29 22:09:55 +0900

    5-3-2. Specify key prop (id) & Add new features using filter()

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,15 @@ export default function App() {
 
   const items = [
     {
+      id: 0, // map() 호출 내부의 JSX 엘리먼트에는 항상 key가 필요
       title: '입문자를 위한, HTML&CSS 웹 개발 입문',
       description: '웹 개발에 필요한 기본 지식을 배웁니다.',
       thumbnail: '/img/htmlcss.png',
       isFavorite: true,
+      link: 'https://inf.run/JxyyT'
     },
     {
+      id: 1, // map() 호출 내부의 JSX 엘리먼트에는 항상 key가 필요
       title: '입문자를 위한, ES6+ 최신 자바스크립트 입문',
       description: '쉽고! 알찬! 내용을 준비했습니다.',
       thumbnail: '/img/js.png',
@@ -18,6 +21,7 @@ export default function App() {
       link: 'https://inf.run/Kpnd'
     },
     {
+      id: 2, // map() 호출 내부의 JSX 엘리먼트에는 항상 key가 필요
       title: '포트폴리오 사이트 만들고 배포까지!',
       description: '포트폴리오 사이트를 만들고 배포해 보세요.',
       thumbnail: '/img/portfolio.png',
@@ -26,10 +30,12 @@ export default function App() {
     }
   ];
 
+  const favoriteItems = items.filter(item => item.isFavorite);
   return (
     <>
-      <main>
-        <CourseListCard items={items} />
+      <main style={{ flexDirection: 'column', gap: '1rem' }}>
+        <CourseListCard title="강의 목록" items={items} />
+        <CourseListCard title="관심 강의" items={favoriteItems} />
       </main>
     </>
   )
--- src/components/course/CourseListCard.jsx
+++ src/components/course/CourseListCard.jsx
@@ -1,15 +1,15 @@
 import Card from '../Card';
 import CourseItem from './CourseItem'
 
-function CourseListCard({ items }) { // 객체 구조 분해
+function CourseListCard({ title, items }) { // 객체 구조 분해
 
   const [course1, course2, course3] = items; // 배열 구조 분해
 
   return (
     <>
-      <Card title="강의 목록">
+      <Card title={title}>
         <div className="courses">
-          {items.map(item => <CourseItem {...item} />)}
+          {items.map(item => <CourseItem key={item.id} {...item} />)}
         </div>
       </Card>
     </>
