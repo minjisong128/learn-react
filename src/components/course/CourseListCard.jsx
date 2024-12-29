d23a8661f6cd5ea25314b20ae94e95fe92b543e3
Minji Song <minjisong128@gmail.com>
2024-12-29 21:48:27 +0900

    5-3-1. Simplify with map()

--- src/components/course/CourseListCard.jsx
+++ src/components/course/CourseListCard.jsx
@@ -6,14 +6,10 @@ function CourseListCard({ items }) { // 객체 구조 분해
   const [course1, course2, course3] = items; // 배열 구조 분해
 
   return (
-    // style={{backgroundColor: 'black', color: 'white'}}
     <>
       <Card title="강의 목록">
         <div className="courses">
-          {/* JSX spread 문법 활용 */}
-          <CourseItem {...course1} />
-          <CourseItem {...course2} />
-          <CourseItem {...course3} />
+          {items.map(item => <CourseItem {...item} />)}
         </div>
       </Card>
     </>
