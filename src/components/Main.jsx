21b6258fb6b1e1bc88de06c8f6cfd230bcca54d6
Minji Song <minjisong128@gmail.com>
2024-12-30 01:33:23 +0900

    6-1-2. Add the feature to show total clicks

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,16 +1,25 @@
+import { useState } from "react";
 import Counter from "./Counter";
 
 export default function Main() {
 
+  const [total, setTotal] = useState(0);
+
+  // total 값 추가하기 위한 이벤트 핸들러
+  const handleTotal = () => {
+    setTotal(total + 1);
+  }
+
   return (
     <main>
+      <h2>total: {total}</h2>
       {/* Counter 컴포넌트를 두 번 렌더링함 */}
       {/* 한쪽 counter 값이 증가한다고 해서 다른 쪽 counter 값이 증가하지 않음
           -> useState를 사용해서 만들어진 컴포넌트 메모리는 컴포넌트 인스턴스별로 관리됨 */}
-      <Counter />
+      <Counter onTotal={handleTotal} />
       <br />
       <br />
-      <Counter />
+      <Counter onTotal={handleTotal} />
     </main>
   )
 }
