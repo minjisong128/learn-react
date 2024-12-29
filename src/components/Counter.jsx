71f44f0ca35bc79fdc3d284dbd3c1e1ccec96b2c
Minji Song <minjisong128@gmail.com>
2024-12-30 01:25:00 +0900

    6-1-1. Increment counter on button click

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,5 +1,21 @@
+import { useState } from "react"; // 훅 : React에서 useState와 같이 “use”로 시작하는 다른 모든 함수
+
 export default function Counter() {
+  // cf) 이대로 작성하면 변경되는 Counter 값이 화면에 렌더링되지 않음
+  // let counter = 1;
+  // const handleCounter = () => {
+  //   counter++;
+  // }
+
+  // useState의 첫 번째 인자 : 초깃값
+  // 반환된 배열의 첫 번째 인자 : 컴포넌트 상태 / 두 번째 인자 : 컴포넌트 상태를 변경할 수 있는 함수
+  const [counter, setCounter] = useState(0);
+
+  const handleCounter = () => {
+    setCounter(counter + 1);
+  }
+
   return (
-    <button>Counter</button>
+    <button onClick={handleCounter}>Counter: {counter}</button>
   )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,16 @@
 import Counter from "./Counter";
 
 export default function Main() {
+
   return (
-    <Counter />
+    <main>
+      {/* Counter 컴포넌트를 두 번 렌더링함 */}
+      {/* 한쪽 counter 값이 증가한다고 해서 다른 쪽 counter 값이 증가하지 않음
+          -> useState를 사용해서 만들어진 컴포넌트 메모리는 컴포넌트 인스턴스별로 관리됨 */}
+      <Counter />
+      <br />
+      <br />
+      <Counter />
+    </main>
   )
 }
