801a9ed8eb34c0796f279de37e88aea498867a35
Minji Song <minjisong128@gmail.com>
2024-12-29 22:43:03 +0900

    5-3-3. Add seperators using <Fragment>

--- src/components/course/CourseListCard.jsx
+++ src/components/course/CourseListCard.jsx
@@ -1,15 +1,23 @@
+import { Fragment } from 'react'; // React에서는 여러 개의 DOM 노드를 렌더링하고 싶을 때 Fragment 활용
 import Card from '../Card';
 import CourseItem from './CourseItem'
 
-function CourseListCard({ title, items }) { // 객체 구조 분해
+function CourseListCard({ title, items }) {
 
-  const [course1, course2, course3] = items; // 배열 구조 분해
+  const lastIndex = items.length - 1;
 
   return (
     <>
       <Card title={title}>
         <div className="courses">
-          {items.map(item => <CourseItem key={item.id} {...item} />)}
+          {items.map((item, index) => (
+            // 리스트 안에서 자식 컴포넌트 렌더링할 때는 key가 필요
+            <Fragment key={item.id}>
+              <CourseItem {...item} />
+              {/* 마지막 항목에 대해서는 구분선 없애기 위한 조건부 렌더링 */}
+              {index !== lastIndex && <hr className="divider" />}
+            </Fragment>
+          ))}
         </div>
       </Card>
     </>
