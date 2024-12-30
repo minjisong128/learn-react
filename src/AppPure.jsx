4c88fb1886e16a51aaca16fc03d00060a93c3b44
Minji Song <minjisong128@gmail.com>
2024-12-30 11:17:06 +0900

    6-3-3. Impure component - Print logs to check for additional re-rendering of components in Strict Mode

--- src/AppPure.jsx
+++ src/AppPure.jsx
@@ -1,13 +1,13 @@
 import './App.css';
-import PullUpPure from './components/PullUpPure';
+import PullUpImpure from './components/PullUpImpure';
 
 function AppPure(props) {
   return (
     <div>
       {/* 순수한 컴포넌트 : 동일한 입력에 대해 동일한 JSX 반환 */}
-      <PullUpPure counter={11} />
-      <PullUpPure counter={12} />
-      <PullUpPure counter={13} />
+      <PullUpImpure />
+      <PullUpImpure />
+      <PullUpImpure />
     </div>
   );
 }
