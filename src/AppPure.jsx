7428516ba45ec9d6085c106c2beef5cfc5760dc9
Minji Song <minjisong128@gmail.com>
2024-12-30 10:51:50 +0900

    6-3-2. New example - Pure component

--- src/AppPure.jsx
+++ src/AppPure.jsx
@@ -1,12 +1,13 @@
 import './App.css';
-import PullUpImpure from './components/PullUpImpure';
+import PullUpPure from './components/PullUpPure';
 
 function AppPure(props) {
   return (
     <div>
-      <PullUpImpure />
-      <PullUpImpure />
-      <PullUpImpure />
+      {/* 순수한 컴포넌트 : 동일한 입력에 대해 동일한 JSX 반환 */}
+      <PullUpPure counter={11} />
+      <PullUpPure counter={12} />
+      <PullUpPure counter={13} />
     </div>
   );
 }
