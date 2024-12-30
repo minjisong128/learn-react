d07e0eb8b8b114858717efbed3c10cc5efd4097c
Minji Song <minjisong128@gmail.com>
2024-12-30 17:58:55 +0900

    7-4-3. Add checkbox & delete line feature by changing items in the array

--- src/AppTodo.jsx
+++ src/AppTodo.jsx
@@ -5,8 +5,8 @@ import TodoList from './components/todo/TodoList'
 function AppTodo(props) {
   const [todoText, setTodoText] = useState('');
   const [todos, setTodos] = useState([
-    { id: 0, text: 'HTML&CSS 공부하기' },
-    { id: 1, text: '자바스크립트 공부하기' }
+    { id: 0, text: 'HTML&CSS 공부하기', done: false },
+    { id: 1, text: '자바스크립트 공부하기', done: false }
   ])
 
   const handleTodoTextChange = (e) => {
@@ -18,7 +18,7 @@ function AppTodo(props) {
     // cf) todos.push({ id: nextId, text: '' }) 와 같이 작성 시 추가 안 됨
     setTodos([
       ...todos,
-      { id: nextId, text: todoText }
+      { id: nextId, text: todoText, done: false }
     ]);
     setTodoText(''); // 추가 버튼 클릭 후 텍스트 초기화 위한 코드 cf) 빈 문자열 말고 null이나 undefined로 작성 시 동작 안 함
   }
@@ -28,15 +28,39 @@ function AppTodo(props) {
     setTodos(newTodos);
   }
 
+  // 텍스트 입력 후 Enter 키 눌렀을 때에도 추가 버튼 눌렀을 때와 동일하게 항목 추가
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      handleAddTodo();
+    }
+  }
+
+  // checkbox 클릭 시 상태 변경
+  const handleToggleTodo = (id, done) => {
+    // 기존 배열 안의 객체 속성을 변경
+    const nextTodos = todos.map(item => {
+      if (item.id === id) {
+        return { ...item, done }; // return { ...item, done: done };을 간략히 작성
+      }
+      return item;
+    })
+    setTodos(nextTodos);
+  }
+
   return (
     <div>
       <h2>할일목록</h2>
-      <input type="text" value={todoText} onChange={handleTodoTextChange} />
+      <input
+        type="text"
+        value={todoText}
+        onChange={handleTodoTextChange}
+        onKeyDown={handleKeyDown} />
       <button onClick={handleAddTodo}>추가</button>
       <div>Preview: {todoText}</div>
       <TodoList
         todos={todos}
         onDeleteTodo={handleDeleteTodo}
+        onToggleTodo={handleToggleTodo}
       />
     </div>
   );
--- src/components/todo/TodoList.jsx
+++ src/components/todo/TodoList.jsx
@@ -1,9 +1,13 @@
-function TodoList({ onDeleteTodo, todos = [] }) {
+function TodoList({ onToggleTodo, onDeleteTodo, todos = [] }) {
   return (
     <ul>
       {todos.map(item => (
         <li key={item.id}>
-          <span>{item.text}</span>
+          <input
+            type="checkbox"
+            checked={item.done}
+            onChange={(e) => onToggleTodo(item.id, e.target.checked)} />
+          <span>{item.done ? (<del>{item.text}</del>) : item.text}</span>
           <button onClick={() => onDeleteTodo(item.id)}>X</button>
         </li>
       ))}
