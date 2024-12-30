ff4759df20599f691d6f0036688644fc6d950dbc
Minji Song <minjisong128@gmail.com>
2024-12-30 16:04:07 +0900

    7-4-1. Add text inputs to the array when clicking the button

--- src/AppTodo.jsx
+++ src/AppTodo.jsx
@@ -3,14 +3,31 @@ import './App.css';
 import TodoList from './components/todo/TodoList'
 
 function AppTodo(props) {
+  const [todoText, setTodoText] = useState('');
   const [todos, setTodos] = useState([
-    { id: 0, label: 'HTML&CSS 공부하기' },
-    { id: 1, label: '자바스크립트 공부하기' }
+    { id: 0, text: 'HTML&CSS 공부하기' },
+    { id: 1, text: '자바스크립트 공부하기' }
   ])
+
+  const handleTodoTextChange = (e) => {
+    setTodoText(e.target.value);
+  }
+
+  const handleAddTodo= () => {
+    const nextId = todos.length;
+    // cf) todos.push({ id: nextId, text: '' }) 와 같이 작성 시 추가 안 됨
+    setTodos([
+      ...todos,
+      { id: nextId, text: todoText }
+    ]);
+    setTodoText(''); // 추가 버튼 클릭 후 텍스트 초기화 위한 코드 cf) 빈 문자열 말고 null이나 undefined로 작성 시 동작 안 함
+  }
   return (
     <div>
       <h2>할일목록</h2>
-      <TodoList todos={todos} />
+      <input type="text" value={todoText} onChange={handleTodoTextChange} />
+      <button onClick={handleAddTodo}>추가</button>
+      <div>Preview: {todoText}</div>
       <TodoList todos={todos} />
     </div>
   );
--- src/components/todo/TodoList.jsx
+++ src/components/todo/TodoList.jsx
@@ -1,12 +1,7 @@
 function TodoList({ todos = [] }) {
-  // 순수한 컴포넌트로 만들기 위해서는 items에 대한 깊은 복사(Deep Copy) 필요
-  // 깊은 복사(Deep Copy)하지 않으면 AppTodo.jsx에서 TodoList 컴포넌트 여러 개 활용 시 items에 누적하여 push됨 (todos 내에 id값이 2인 요소가 여러 개 될 수 있음)
-  const items = [...todos]; // 깊은 복사(Deep Copy)
-  items.push({ id: 2, label: '포트폴리오 사이트 만들기' });
-
   return (
     <ul>
-      {items.map(item => <li key={item.id}>{item.label}</li>)}
+      {todos.map(item => <li key={item.id}>{item.text}</li>)}
     </ul>
   )
 }
