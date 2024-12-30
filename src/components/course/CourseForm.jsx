a721684214cac4915fec24f22e9674edd5fcc70d
Minji Song <minjisong128@gmail.com>
2024-12-30 14:46:46 +0900

    7-1. Add AppMovingDot.jsx & course registration feature

--- src/components/course/CourseForm.jsx
+++ src/components/course/CourseForm.jsx
@@ -1,17 +1,37 @@
+import { useState } from "react";
 import Card from "../Card";
 
 export default function CourseForm() {
+
+  const [form, setForm] = useState({
+    title: '리액트 강의',
+    description: '리액트 기초부터 실전까지!'
+  })
 
   function handleCourseForm(e) { // 이 이벤트 핸들러 없으면 '등록' 버튼 클릭 시 페이지 reload됨
     e.preventDefault(); // 기본 동작 막음
   }
 
+  const handleChange = (e) => {
+    console.log('e.target.name: ', e.target.name);
+    setForm({
+      ...form, // 전개 구문 통해 [e.target.name](e.g. title) 외 기존 속성(e.g. description) 유지
+      [e.target.name]: e.target.value // [e.target.name] : title or description이 동적으로 할당됨
+    })
+  }
+
   return (
     <Card title="강의 등록">
       <form style={{ display:'flex', flexDirection: 'column', gap: '1rem' }} onSubmit={handleCourseForm}>
-        <input type="text" placeholder="강의 제목" />
-        <input type="text" placeholder="강의 한줄 설명" />
+        <input name="title" type="text" placeholder="강의 제목" value={form.title} onChange={handleChange} />
+        <input name="description" type="text" placeholder="강의 한줄 설명" value={form.description} onChange={handleChange} />
         <input id="submit" type="submit" value="등록" />
+        {(form.title || form.description) && (
+          <div style={{marginTop: '16px', padding: '16px', backgroundColor: '#eee', borderRadius: '6px' }}>
+            {form.title && (<p>제목 - {form.title}</p>)}
+            {form.description && (<p>설명 - {form.description}</p>)}
+          </div>
+        )}
       </form>
     </Card>
   )
