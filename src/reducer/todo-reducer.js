ea9dd0ec989a513fa90623c394495081b38b04ef
Minji Song <minjisong128@gmail.com>
2024-12-31 00:13:01 +0900

    8-2. Write concise reducers with Immer

--- src/reducer/todo-reducer.js
+++ src/reducer/todo-reducer.js
@@ -1,40 +1,35 @@
 // 분산된 비즈니스 로직을 하나로 통합
 // 첫 번째 인자 : 관리할 상태
 // 두 번째 인자 : AppTodo.jsx에서 dispatch 함수로 넘겨준 action 객체
-export default function todoReducer(todos, action) {
+
+// useImmerReducer 활용 시 수정 가능한 객체(여기서는 draft) 넘어옴
+// Immer 내부적으로 새로운 객체 생성
+// cf) useReducer 사용 시 첫 번째 인자 수정 불가
+export default function todoReducer(draft, action) {
   // type
   switch(action.type) { // reducer 함수 안에서는 switch 문을 사용하는 게 규칙
     case 'added': {
       const { nextId, todoText } = action;
-      // 기존에는 setter 계열 메서드로 값을 전달했다면, 여기서는 값을 저장하는 대신 return하면 됨
-      return [
-        ...todos,
-        { id: nextId, text: todoText, done: false }
-      ];
+      draft.push({ id: nextId, text: todoText, done: false });
+      break;
     }
     case 'added_index': {
       const { insertAt, nextId, todoText } = action;
-      return [
-        ...todos.slice(0, insertAt), // 삽입 지점 이전 항목
-        { id: nextId, text: todoText, done: false },
-        ...todos.slice(insertAt) // 삽입 지점 이후 항목
-      ];
+      draft.splice(insertAt, 0, { id: nextId, text: todoText, done: false });
+      break;
     }
     case 'deleted': {
       const { deleteId } = action;
-      return todos.filter(item => item.id !== deleteId);
+      return draft.filter(item => item.id !== deleteId);
     }
     case 'done': {
       const { id, done } = action;
-      return todos.map(item => {
-        if (item.id === id) {
-          return { ...item, done }; // return { ...item, done: done };을 간략히 작성
-        }
-        return item;
-      })
+      const target = draft.find(item => item.id === id);
+      target.done = done;
+      break;
     }
     case 'reverse': {
-      return todos.toReversed();
+      return draft.toReversed();
     }
     default: {
       throw Error('알 수 없는 액션 타입: ' + action.type);
