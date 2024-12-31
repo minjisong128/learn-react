39116b916080758468791796915743d65322ec28
Minji Song <minjisong128@gmail.com>
2024-12-31 15:38:02 +0900

    10-3-2. Simplify features & separate into components

--- src/AppTodo.jsx
+++ src/AppTodo.jsx
@@ -2,53 +2,28 @@ import { useReducer, useState } from 'react';
 import './App.css';
 import TodoList from './components/todo/TodoList'
 import todoReducer from './reducer/todo-reducer';
+import AddTodo from './components/todo/AddTodo';
 import { useImmerReducer } from 'use-immer';
 
 function AppTodo(props) {
-  const [todoText, setTodoText] = useState('');
+
   const [todos, dispatch] = useImmerReducer(todoReducer, [
     { id: 0, text: 'HTML&CSS 공부하기', done: false },
     { id: 1, text: '자바스크립트 공부하기', done: false }
   ]);
 
-  const [insertAt, setInsertAt] = useState(todos.length - 1);
-
-  const handleTodoTextChange = (e) => {
-    setTodoText(e.target.value);
-  }
-
   //1] added
-  const handleAddTodo= () => {
+  const handleAddTodo= (text) => {
     dispatch({ // reducer(todo-reducer.js)에 dispatch 함수로 action 객체 넘김
       type: 'added',
       nextId: todos.length,
-      todoText
+      todoText: text
     });
-    setTodoText(''); // 추가 버튼 클릭 후 텍스트 초기화 위한 코드 cf) 빈 문자열 말고 null이나 undefined로 작성 시 동작 안 함
-  }
-
-  //2] added_index
-  const handleAddTodoByIndex = () => {
-    dispatch({
-      type: 'added_index',
-      insertAt,
-      nextId: todos.length,
-      todoText
-    });
-    setTodoText('');
   }
 
   //3] deleted
   const handleDeleteTodo = (deleteId) => {
     dispatch({ type: 'deleted', deleteId });
-  }
-
-  // 텍스트 입력 후 Enter 키 눌렀을 때에도 추가 버튼 눌렀을 때와 동일하게 항목 추가
-  const handleAddTodoOnEnter = (e) => {
-    // 조합중일 때(e.nativeEvent.isComposing가 true일 때) 추가 버튼 클릭 시 두 개 추가되는 문제 해결
-    if (e.key === 'Enter' && e.nativeEvent.isComposing === false) {
-      handleAddTodo();
-    }
   }
 
   //4] done (완료 처리)
@@ -57,32 +32,12 @@ function AppTodo(props) {
     dispatch({ type: 'done', id, done });
   }
 
-  //5] reverse
-  const handleReverse = () => {
-    dispatch({ type: 'reverse' })
-  }
-
   return (
     <div>
       <h2>할일목록</h2>
-      <div>
-        <input
-          type="text"
-          value={todoText}
-          onChange={handleTodoTextChange}
-          onKeyDown={handleAddTodoOnEnter} />
-        <button onClick={handleAddTodo}>추가</button>
-      </div>
-      <div>
-        <select value={insertAt} onChange={(e) => setInsertAt(e.target.value)}>
-          {todos.map((item, index) => (
-            <option key={item.id} value={index}>{index} 번째</option>
-          ))}
-        </select>
-        <button onClick={handleAddTodoByIndex}>{insertAt}번째 추가</button>
-      </div>
-      <div>Preview: {todoText}</div>
-      <button onClick={handleReverse}>Reverse</button>
+      <AddTodo
+        onAddTodo={handleAddTodo}
+      />
       <TodoList
         todos={todos}
         onDeleteTodo={handleDeleteTodo}
---
+++ src/components/todo/AddTodo.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+export default function AddTodo({ onAddTodo }) {
+  const [todoText, setTodoText] = useState('');
+  return (
+    <div>
+      <input
+        type="text"
+        value={todoText}
+        onChange={(e) => setTodoText(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter' && e.nativeEvent.isComposing === false) {
+            onAddTodo(e.target.value);
+            setTodoText('');
+          }
+        }}
+      />
+      <button onClick={() => {
+        // 저장
+        setTodoText('');
+        onAddTodo(todoText);
+      }}>추가</button>
+    </div>
+  )
+}
