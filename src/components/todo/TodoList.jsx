6eb8e865c64296b0c148fed4bdc557bf67f98bce
Minji Song <minjisong128@gmail.com>
2024-12-31 15:17:09 +0900

    10-3-1. Separate into components

--- src/components/todo/TodoList.jsx
+++ src/components/todo/TodoList.jsx
@@ -1,14 +1,15 @@
+import TodoItem from "./TodoItem";
+
 function TodoList({ onToggleTodo, onDeleteTodo, todos = [] }) {
   return (
     <ul>
       {todos.map(item => (
         <li key={item.id}>
-          <input
-            type="checkbox"
-            checked={item.done}
-            onChange={(e) => onToggleTodo(item.id, e.target.checked)} />
-          <span>{item.done ? (<del>{item.text}</del>) : item.text}</span>
-          <button onClick={() => onDeleteTodo(item.id)}>X</button>
+          <TodoItem
+            item={item}
+            onToggleTodo={onToggleTodo}
+            onDeleteTodo={onDeleteTodo}
+          />
         </li>
       ))}
     </ul>
